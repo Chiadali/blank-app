2f3d5ae85d089e1909ca0873aff739acf8fe23b1
Chiadali <chiadali29@gmail.com>
2025-03-19 12:42:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.title("La Mia App Streamlit")
+st.write("Benvenuto nella mia app!")
+
+if st.button("Clicca qui"):
+    st.write("Hai cliccato il pulsante!")
